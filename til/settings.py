5e41bb641ada91fc1b362d4803ac8c1e7594dc2d
Max Mercel <mmercel8@gmail.com>
2021-10-26 10:44:19 +0100

    Modified settings.py and urls to .py so profile pictures are accessible via /media/profiles/ , added /media/ to gitignore

--- til/settings.py
+++ til/settings.py
@@ -17,6 +17,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_URL = "/media/"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -45,7 +48,8 @@ INSTALLED_APPS = [
     'profiles',
     'allauth',
     'allauth.account',
-    'allauth.socialaccount'
+    'allauth.socialaccount',
+    'sorl.thumbnail'
 ]
 
 MIDDLEWARE = [
