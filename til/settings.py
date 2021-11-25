81c618b0c09b7f41598b7866d1f254d286ef3a1c
Max Mercel <mmercel8@gmail.com>
2021-11-25 14:06:19 +0000

    Set up static root and URL, added frontend JS file that outputs Hello World to console as test

--- til/settings.py
+++ til/settings.py
@@ -160,3 +160,10 @@ AUTHENTICATION_BACKENDS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, "frontend/")
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
