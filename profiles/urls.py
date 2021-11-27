515ced5e0a586f640970b5c0eb973810980dbf8e
Max Mercel <mmercel8@gmail.com>
2021-11-27 12:44:40 +0000

    Added profile detail view and entry in urls.py, edited post.html template to go to the profile detail url

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,1 +1,8 @@
 from django.urls import path
+from . import views 
+
+app_name = "profiles"
+
+urlpatterns = [
+     path("profile/<str:username>/", views.ProfileDetailView.as_view(), name = "detail")
+]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, UserManager
+from django.views.generic import DetailView
 
-# Create your views here.
+class ProfileDetailView(DetailView):
+    http_method_names=["GET"]
+    template_name = "profiles/detail.html"
+    model = User
+    context_object_name = "user"
+    slug_field = "username"
+    slug_url_kwarg = "username"
